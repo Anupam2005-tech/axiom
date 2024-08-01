4e16072b9a2c73e28f26bcfc88439fb66f3eaacd
Anupam Bhowmik <abhowmik068@gmail.com>
2024-08-02 01:45:22 +0530

    Added the searchbar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function typeText() {
   if (index < text.length) {
     typingText.innerHTML += text.charAt(index);
     index++;
-    setTimeout(typeText, 200); // adjust the speed (100ms)
+    setTimeout(typeText, 100); 
   }
 }
 
