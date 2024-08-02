e20be8336c07de248850f44a42cb762fe3c83c6b
Anupam Bhowmik <abhowmik068@gmail.com>
2024-08-02 15:26:18 +0530

    blur effect

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ function typeText() {
   if (index < text.length) {
     typingText.innerHTML += text.charAt(index);
     index++;
-    setTimeout(typeText, 100); 
+    setTimeout(typeText, 100);
   }
 }
 
-typeText();
 
+typeText();
+// let canvas = document.querySelector("#canvas");
+// let ctx = canvas.getContext("2d");
+
+
+// ctx.arc(165, 75, 60, 0, Math.PI * 2, false);
+// ctx.shadowColor = "white";
+// ctx.shadowBlur = 10;
+// ctx.strokeStyle = "white";
+// ctx.lineWidth = 5;
+
+// ctx.stroke();
